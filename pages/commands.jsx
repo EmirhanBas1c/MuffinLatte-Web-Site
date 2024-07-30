64ce317a1dbd0fe67d8cf7dfaae02fc26387664e
EmirhanBas1c <123837275+EmirhanBas1c@users.noreply.github.com>
2024-07-30 09:33:10 +0300

    Update commands.jsx

--- pages/commands.jsx
+++ pages/commands.jsx
@@ -3,32 +3,34 @@ import Link from "next/link";
 
 export default function Commands() {
 const commands = [
-  "/autoplay",
-  "/back",
-  "/statistic",
-  "/channel",
-  "/clear",
-  "/dj",
-  "/filter",
-  "/help",
-  "/language",
-  "/loop",
-  "/nowplaying",
-  "/pause",
+  "/yardım",
+  "/afk",
+  "/avatar",
+  "/ayarlar",
+  "/ban",
+  "/ban-list",
+  "/banner",
+  "/davet",
+  "/forceban",
+  "/istatistik",
+  "/kanal-kilitle",
+  "/kick",
+  "/kullanıcı-bilgi",
+  "/kurucu-kim",
+  "/küfür-engel",
+  "/mod-log",
+  "/mute",
+  "/oylama",
   "/ping",
-  "/play",
-  "/playlist",
-  "/queue",
-  "/resume",
-  "/save",
-  "/search",
-  "/seek",
-  "/servers",
-  "/shuffle",
-  "/skip",
-  "/stop",
-  "/time",
-  "/volume"
+  "/random-anime",
+  "/reklam-engel",
+  "/say",
+  "/sil",
+  "/sunucu-bilgi",
+  "/warn",
+  "/yasaklı-kelime",
+  "/yavaş-mod",
+  "Ve Dahası..."
 ];
 
   return (
@@ -42,7 +44,7 @@ const commands = [
          Bot Commands
         </p>
         <p className="text-white text-sm text-opacity-50 mb-5">
-        You can get information about the commands of the MusicMaker Bot.
+        You can get information about the commands of the MuffinLatte Bot.
         </p>
 
         <div>
@@ -91,12 +93,12 @@ const commands = [
   
   <Link href={"https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=applications.commands%20bot"}>
   <a className={"flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Invite MusicMaker</a>
+Invite MuffinLatte</a>
   </Link>
 
   <div className="py-10"></div>
   
-  <Link href={"https://discord.gg/ZcDprCrFTn"}>
+  <Link href={"https://discord.gg/cPjJdfbajV"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Support Server</a>
   </Link>
@@ -105,7 +107,7 @@ Support Server</a>
   
   <Link href={"https://top.gg/bot/774043716797071371/vote"}>
   <a className={" px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
-Vote MusicMaker</a>
+Vote MuffinLatte</a>
   </Link>
 </div>
 
