8972c83130b2097f9caffa7d78520b993446306f
EmirhanBas1c <123837275+EmirhanBas1c@users.noreply.github.com>
2024-08-02 09:53:51 +0300

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -17,7 +17,7 @@ export default function Index() {
 	  </p>
         <div className="animateHeader mt-10 flex flex-wrap items-center justify-center gap-x-4">
   
-            <Link href={"https://discord.com/api/oauth2/authorize?client_id=774043716797071371&permissions=277028620608&scope=applications.commands%20bot"}>
+            <Link href={"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=bot+su+anda+bakımda+oldugu+ıcın+davet+edılemez"}>
               <a
                 className={
                   "flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " 
