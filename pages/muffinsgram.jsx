2381d7a67957b8bc131af99d2ca43d29bc26095c
EmirhanBas1c <123837275+EmirhanBas1c@users.noreply.github.com>
2024-08-22 11:24:23 +0300

    Update muffinsgram.jsx

--- pages/muffinsgram.jsx
+++ pages/muffinsgram.jsx
@@ -18,7 +18,7 @@ export default function Muffinsgram() {
             <div className="flex flex-col justify-center text-white rounded">
             <a href="https://biyogra.fi/Muffinsgram">
                         <div className="flex-1 gap-x-4 flex items-center bg-gradient-to-b from-neutral-900/80 to-neutral-900/20 p-3 rounded-full">
-                            <img className="rounded-full h-32 w-32" src="https://imgur.com/X7Cbv8A" />
+                            <img className="rounded-full h-32 w-32" src="https://i.imgur.com/X7Cbv8A.png" />
                             <div>
                                 <h1 className="leading-none text-3xl font-bold text-white">Muffinsgram</h1>        
                                 <div className="flex items-center mt-1">
@@ -30,7 +30,6 @@ export default function Muffinsgram() {
                         </div>
                     </a>
                     </div>
-
 
                 </div>
                 <div className="py-10"></div>
